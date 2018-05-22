44d7e04b22a42cc42f201fabccad889725abbdc2
satyamjay <Satyamisgod@gmail.com>
2018-05-22 22:04:57 +0530

    deployment

--- BuildMyShip/settings.py
+++ BuildMyShip/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static1/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
