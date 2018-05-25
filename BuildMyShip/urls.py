15cb399ee9bd7b96006f64d8b25ef5b9b6b88a22
satyamjay <Satyamisgod@gmail.com>
2018-05-25 08:15:40 +0530

    fixed a broken link

--- BuildMyShip/urls.py
+++ BuildMyShip/urls.py
@@ -37,6 +37,10 @@ urlpatterns = [
     path('api/like_answer/<int:answer_id>/', views.LikeAnswer.as_view()),
     path('api/report_answer/<int:answer_id>/', views.ReportAnswer.as_view()),
     re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
+	# List My Questions
+    path('my_questions/', views.my_questions),
+    # Delete my question
+    path('delete_question/<int:question_id>', views.delete_question)
 
 
 ]
